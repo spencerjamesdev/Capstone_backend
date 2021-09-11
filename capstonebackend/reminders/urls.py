5102d46b9a41f444d7f05d317bbf4253e339bbd9
spencerjamesdev <spencer.franke@gmail.com>
2021-09-10 20:43:20 -0500

    Added GetById, Put Delete for reminders  rest api

--- capstonebackend/reminders/urls.py
+++ capstonebackend/reminders/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('reminders/', views.ReminderList.as_view()),
+    path('reminders/<int:pk>/', views.ReminderDetail.as_view()),
 ]
--- capstonebackend/reminders/views.py
+++ capstonebackend/reminders/views.py
@@ -1,3 +1,4 @@
+from django.http.response import Http404
 from django.shortcuts import render
 from .models import Reminder
 from .serializers import ReminderSerializer
@@ -20,3 +21,30 @@ class ReminderList(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class ReminderDetail(APIView):
+
+    def get_object(self, pk):
+        try:
+            return Reminder.objects.get(pk=pk)
+        except Reminder.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        reminder = self.get_object(pk)
+        serializer = ReminderSerializer(reminder)
+        return Response(serializer.data)
+
+    def put(self, request, pk):
+        reminder = self.get_object(pk)
+        serializer = ReminderSerializer(reminder, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk):
+        reminder = self.get_object(pk)
+        reminder.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
